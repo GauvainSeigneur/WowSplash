7804211a4d106aee6064f6dfccef795a6d3d4636
Gauvain Seigneur <gauvain.seigneur@gmail.com>
2019-09-12 22:35:25 +0200

    feat(User): add presenter

--- app/src/main/java/com/seigneur/gauvain/wowsplash/ui/user/UserViewModel.kt
+++ app/src/main/java/com/seigneur/gauvain/wowsplash/ui/user/UserViewModel.kt
@@ -8,9 +8,10 @@ import com.seigneur.gauvain.wowsplash.data.repository.UserRepository
 import com.seigneur.gauvain.wowsplash.ui.base.BaseViewModel
 import timber.log.Timber
 
-class UserViewModel(userRepository: UserRepository) : BaseViewModel(), UserInteractor.UserCallback {
+class UserViewModel(userRepository: UserRepository) : BaseViewModel(), UserPresenter {
 
     val mUserResult = MutableLiveData<UserResult>()
+
     val mUserInteractor by lazy {
         UserInteractor(userRepository, mDisposables, this)
     }
